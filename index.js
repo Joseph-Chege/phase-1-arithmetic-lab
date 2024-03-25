7b116bb0e484617f34d00d0f071e65c4ea2cace2
Joseph-Chege <mjchege23@gmail.com>
2024-03-25 22:19:53 +0300

    Wrote arithmetic variables

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+
+let num1 = 1;
+let num2 = 62;
+let num3 = 6; 
+let num4 = 10; 
+
+const multiply = num1 * num2 
+
+const random  = Math.floor(Math.random() * 10) + 1;
+
+const mod = num4 % num3;
+
+const max = Math.max(12, 10, 13, 16, 20);
